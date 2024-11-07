85530657c6bb6e5ddc4d521b73449d07c2d68689
adrians5j <adrian@webiny.com>
2024-11-07 15:01:47 +0100

    ci: detect changed packages [no ci]

--- .github/workflows/wac/utils/runNodeScripts/listChangedPackages.js
+++ .github/workflows/wac/utils/runNodeScripts/listChangedPackages.js
@@ -1,12 +1,13 @@
-const detectedChangedFilesStepOutputPath = JSON.parse(
-    "${{ steps.detect-changed-files.outputs.changed_files }}"
-);
+const detectedChangedFilesStepOutputPath =
+    "${{ steps.detect-changed-files.outputs.changed_files }}";
+
 const detectedChangedFiles = JSON.parse(detectedChangedFilesStepOutputPath);
 
 const changedPackages = detectedChangedFiles
     .filter(path => path.startsWith("packages/"))
     .reduce((acc, item) => {
-        const [,packageName] = item.split("/");1
+        const [, packageName] = item.split("/");
+        1;
         acc.add(packageName);
         return acc;
     }, new Set());
