da17409a61690e5af206e70f77a5fc5b09f9ec41
Adrian Smijulj <adrian1358@gmail.com>
2024-11-20 10:36:45 +0100

    fix: add `AddButtonLinkComponent` decorator (#4403)

--- packages/app-page-builder/src/render/PageBuilder.tsx
+++ packages/app-page-builder/src/render/PageBuilder.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AddButtonClickHandlers } from "~/elementDecorators/AddButtonClickHandlers";
+import { AddButtonLinkComponent } from "~/elementDecorators/AddButtonLinkComponent";
 import { InjectElementVariables } from "~/render/variables/InjectElementVariables";
 import { LexicalParagraphRenderer } from "~/render/plugins/elements/paragraph/LexicalParagraph";
 import { LexicalHeadingRenderer } from "~/render/plugins/elements/heading/LexicalHeading";
@@ -7,6 +8,7 @@ import { LexicalHeadingRenderer } from "~/render/plugins/elements/heading/Lexica
 export const PageBuilder = () => {
     return (
         <>
+            <AddButtonLinkComponent />
             <AddButtonClickHandlers />
             <InjectElementVariables />
             <LexicalParagraphRenderer />
