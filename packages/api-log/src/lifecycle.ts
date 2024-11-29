347f9ed736de65dbcd7ea52d840aa07aae585eda
Bruno Zorić <bruno.zoric@gmail.com>
2024-11-29 16:45:42 +0100

    fix(api-log): check for logger instance on context before flushing (#4417)

--- packages/api-log/src/lifecycle.ts
+++ packages/api-log/src/lifecycle.ts
@@ -1,30 +1,36 @@
 import { createModifyFastifyPlugin } from "@webiny/handler";
-import { Context } from "./types";
+import type { Context } from "./types";
 
+const execute = async (input?: Context | unknown) => {
+    if (!input) {
+        return;
+    }
+    const context = input as Context;
+    if (!context.logger?.log?.flush) {
+        return;
+    }
+    // @ts-expect-error
+    else if (context.___flushedLogs) {
+        return;
+    }
+    // @ts-expect-error
+    context.___flushedLogs = true;
+
+    try {
+        await context.logger.log.flush();
+    } catch (ex) {
+        console.error("Error flushing logs.");
+        console.log(ex);
+    }
+};
 export const createLifecycle = () => {
     return createModifyFastifyPlugin(app => {
-        const execute = async () => {
-            // @ts-expect-error
-            if (app.webiny.___flushedLogs) {
-                return;
-            }
-            // @ts-expect-error
-            app.webiny.___flushedLogs = true;
-            const context = app.webiny as Context;
-            try {
-                await context.logger.log.flush();
-            } catch (ex) {
-                console.error("Error flushing logs.");
-                console.log(ex);
-            }
-        };
-
         app.addHook("onTimeout", async () => {
-            execute();
+            execute(app.webiny);
         });
 
         app.addHook("onResponse", async () => {
-            execute();
+            execute(app.webiny);
         });
     });
 };
